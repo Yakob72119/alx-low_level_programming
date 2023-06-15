41075b1566fab781a3f9220b71abca0b11a0415a
yakob <beshayakob72119@gmail.com>
2023-06-15 11:40:03 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print a function using printf
+ *
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	pritf("with proper grammar, but the outcome is
+a piece of art,\n")
+	return (0);
+}
