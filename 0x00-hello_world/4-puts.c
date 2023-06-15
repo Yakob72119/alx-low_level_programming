9fe90da21ffdd13958bd2f490cf46cbf364ef868
yakob <beshayakob72119@gmail.com>
2023-06-15 13:36:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/** 
-  * main - Prints words. 
-  * 
-  * Return: returns 0. 
- */ 
+/**
+ * main - Prints words.
+ *
+ * Return: returns 0 .
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
