45d261e02a5abc8a33d41d1cd6ec896eda9dda7c
yakob <beshayakob72119@gmail.com>
2023-06-15 06:38:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+/**
+ * main - A Program that prints aline with puts functio 
+n 
+ * Returns: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzz
+le");	
+return (0);
+}
+
